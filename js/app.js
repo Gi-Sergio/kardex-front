f4887dceb0b96de28942083f85d3382263f4414d
Andy Rios <andyrios611@gmail.com>
2025-03-28 07:51:59 -0500

    Proveedores terminado

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
 });
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
-});
-sign_up_btn2.addEventListener("click", () => {
-    container.classList.add("sign-up-mode2");
-});
-sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode2");
 });
 function redirectToIndex(event) {
         event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
@@ -22,14 +14,11 @@ function redirectToIndex(event) {
         // Redirige a index.html
         window.location.href = 'index.html';
 }
-document.getElementById("myButton").onclick = function () {
-    window.location.href = "ProductosVerMas.html";
-};
+
 
 const toggleButton = document.createElement("button");
 toggleButton.innerHTML = "&#9776;";  // Icono de hamburguesa
 toggleButton.classList.add("logo-menu-btn");
-document.querySelector(".header").appendChild(toggleButton);
 
 toggleButton.addEventListener("click", () => {
     const sidebar = document.querySelector(".sidebar");
---
+++ js/services/ProviderService.js
@@ -0,0 +1,86 @@
+import AuthUtils from "../utils/authUtils.js";
+
+const API_URL = "http://localhost:8080/providers"; // Cambia esto por tu URL real
+
+export default class ProviderService {
+  static async getAll(page = 0) {
+    
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    const pageSize = 6; // Tamaño de página
+
+    const response = await fetch(`${API_URL}?page=${page}&size=${pageSize}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  }
+
+  static async getById(providerId) {
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    const response = await fetch(`${API_URL}/${providerId}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  }
+
+  static async create(formData) {
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    let response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`
+      },
+      body: formData
+    });
+    return response;
+  }
+
+  static async update(providerId, formData) {
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    const response = await fetch(`${API_URL}/${providerId}`, {
+     method: "PUT",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`
+      },
+      body: formData
+    });
+    return response;
+  }
+
+  static async patch(providerId, quantity) {
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    const response = await fetch(`${API_URL}/${providerId}/quantity?ProviderQuantity=${quantity}`, {
+     method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`
+      }
+    });
+    return response;
+  }
+
+  static async delete(providerId) {
+    if (!AuthUtils.isAuthenticated()) return; 
+
+    const response = await fetch(`${API_URL}/${providerId}`, {
+     method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${AuthUtils.getToken()}`
+      }
+    });
+    return response;
+  }
+}
